ee3842d280641a6f2b2a3e17eceb1dc41b30d256
Nelson Florencio Junior <nflorencio@stefanini.com>
2018-01-02 13:51:29 -0200

    SEÇÃO: 8 Usando Json no nosso App - AULA 27. Alterando a página de feeds para usar json

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -15,6 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
 
+  public objetoFeed = {
+    titulo: "Nelson Júnior",
+    data: "Janeiro 02, 2018",
+    descricao: "Criando APP BBP!!!",
+    qntLikes: 21,
+    qntComments: 3,
+    dataComments: "11h, ago",
+  }
+
   private nomeUsuario: String = 'Nelson Florêncio Júnior';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
