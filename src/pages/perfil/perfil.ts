0b536a7228e4ae884bf8005e008370705eaa101a
Nelson Florencio Junior <nflorencio@stefanini.com>
2018-01-03 14:16:07 -0200

    SEÇÃO: 15 Considerações Finais - AULA 48. Usando o plugin nativo da Câmera

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 /**
  * Generated class for the PerfilPage page.
  *
@@ -12,10 +14,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-perfil',
   templateUrl: 'perfil.html',
+  providers: [
+    Camera,
+  ]
 })
 export class PerfilPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  img = '';
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private camera: Camera,
+  ) {
   }
+
+  tirarFoto() {
+    const options: CameraOptions = {
+      // qualidade
+      quality: 100,
+      // destino da foto
+      // DATA_URL -> foto em formato de texto (enorme)
+      // FILE_URI -> caminho onte esta armazenada a foto no dispositivo
+      // NATIVE_URI -> ~caminho onte esta armazenada a foto no dispositivo
+      destinationType: this.camera.DestinationType.DATA_URL,
+      // extensão da imagem
+      encodingType: this.camera.EncodingType.JPEG,
+      // tipo.: video, imagem ou outro tipo de mida(ex.: gif)
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
 
 }
